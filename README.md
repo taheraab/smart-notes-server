f7aba7fbfe536e33631ae3c8d23f156094c806db
Tahera Bodabhaiwala <tbo@amboss.com>
2023-10-08 14:09:22 +0200

    Add openai route to find keywords

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # smart-notes-server
+
+## OpenAI API access
+
+Create a `.env` file with your API key from your Open AI account
+
+```
+OPENAI_API_KEY=<your key>
+```
--- routes/openai.js
+++ routes/openai.js
@@ -1,9 +1,60 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const OpenAI = require('openai');
 
-// Summarize given text
-router.get('/summarize', (req, res) => {
-  res.send('Some summary')
+const router = express.Router()
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+router.use((req, res, next) => {
+  if (!process.env.OPENAI_API_KEY) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+  next();
+})
+
+// Highlight keywords in given reference text
+router.post('/keywords', async (req, res) => {
+  const referenceText = req.body.referenceText || '';
+  console.log(referenceText);
+  if (referenceText.trim().length === 0) {
+    return res.send({
+      error: true,
+      errorMessage: "Please provide valid reference text"
+    });
+  }
+
+  const messages = [
+    {
+      role: "system",
+      content: "You are a helpful medical professional. Highlight keywords in the content you are provided to a max 20 words."
+    },
+    {
+      role: 'user',
+      content: referenceText
+    }
+  ]
+  
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages,
+    temperature: 0.6,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0
+  });
+
+  console.log(JSON.stringify(response.choices));
+  res.send({
+    error: false,
+    data: response['choices'][0]['message']['content']
+  });
 })
 
 module.exports = router
