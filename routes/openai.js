4cf1a12f786510b5664a77bb941cbd5831ea1331
Tahera Bodabhaiwala <tbo@amboss.com>
2023-10-11 13:14:38 +0200

    Add flowchart generation

--- routes/openai.js
+++ routes/openai.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const {execSync} = require('child_process')
 const OpenAI = require('openai');
 
 const router = express.Router()
@@ -15,10 +17,7 @@ router.use((req, res, next) => {
     });
     return;
   }
-  next();
-})
 
-async function handleRequest(req, res, systemMessage) {
   const referenceText = req.body.referenceText || '';
 
   if (referenceText.trim().length === 0) {
@@ -27,6 +26,12 @@ async function handleRequest(req, res, systemMessage) {
       errorMessage: "Please provide valid reference text"
     });
   }
+  next();
+})
+
+async function handleRequest(req, res, systemMessage) {
+  const referenceText = req.body.referenceText;
+
   const messages = [
     {
       role: "system",
@@ -67,6 +72,45 @@ router.post('/table', async (req, res) => {
   const systemMessage = "You are a helpful medical professional. Generate a table from the content you are provided."
 
   handleRequest(req, res, systemMessage);
+})
+
+// Generate flowchart from given reference text
+router.post('/flowchart', async (req, res) => {
+  const referenceText = req.body.referenceText;
+  
+  const messages = [
+    {
+      role: "system",
+      content: `Behave as \"MermaidGPT\"; for every query the user submits, you are going to create an example of what Mermaid format for the inputted text looks like. Example map for an example topic: \nflowchart TD \nA[Christmas] →|Get money| B(Go shopping) \nB → C{Let me think} \nC →|One| D[Laptop]`
+    },
+    {
+      role: 'user',
+      content: referenceText
+    }
+  ]
+  
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages,
+    temperature: 0.6,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0
+  });
+
+  console.log(JSON.stringify(response.choices));
+  const diagram = response['choices'][0]['message']['content'];
+
+  // convert mermaid diagram to svg
+  fs.writeFileSync('input.mmd', diagram);
+  execSync('npx mmdc -c mermaidConfig.json -i input.mmd -o output.svg');
+  const svgContent = fs.readFileSync('output.svg', "utf-8");
+
+  res.send({
+    error: false,
+    data: svgContent
+  });
 })
 
 module.exports = router
